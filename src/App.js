055519a2f837b1d5f076386bfdafb3bb29eb2d8c
JUN HARY <salah99@naver.com>
2024-12-11 23:50:11 +0900

    font적용완료

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './App.css'; 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Main from "./pages/common/Main";
 import MyMainPage from "./pages/common/MyMainPage";
